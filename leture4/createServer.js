70fb4b857e03441470e359d6093e3eade500accc
seunghaen <tmdgus9701@naver.com>
2023-02-13 18:16:06 +0900

    first create server

--- leture4/createServer.js
+++ leture4/createServer.js
@@ -1,3 +1,11 @@
 const http = require("http");
 
-http.createServer((req, res) => {});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; char=utf-8" });
+    res.write("<h1>Hello node</h1>");
+    res.end("<p>hello server</p>");
+  })
+  .listen(8080, () => {
+    console.log("8080 포트에서 서버를 연결중");
+  });
