ad6b68671a8d01917883d975089fa05c58052ec3
seunghaen <tmdgus9701@naver.com>
2023-02-20 14:35:01 +0900

    router 분리

--- lecture5/app.js
+++ lecture5/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
 
 const app = express();
 app.use(morgan("dev"));
 app.set("port", 3001);
+app.use;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+app.use("/", indexRouter);
+app.use("/user", userRouter);
 
 app.get("./category/:letter", (req, res) => {
   res.send(`${req.params.letter}`);
